03d8c1513f6f4842b352d27df4b3682a64b9676d
Miguel Roncancio <mroncanicio19@gmail.com>
2019-07-03 22:49:49 -0400

    add setState method

--- InputSource/InputSource.js
+++ InputSource/InputSource.js
@@ -2,24 +2,41 @@
  * Implementation of this class is on an ass needed basis, meaning functionality
  * will be implemented as it is needed during the webdriver development
  */
-const uuid = require('uuid/v4');
+const uuid = require('uuid/v1');
+const { InvalidArgument } = require('../Error/errors');
 
 class InputSource {
-  constructor(type) {
+  constructor(type, subtype = '') {
     this.id = uuid();
     this.type = type;
-    this.actions = {};
-    this.state = {};
+    this.actions = { pause: 0 };
+    this.setState(type, subtype);
   }
 
-  setActions(actionType) {
-    switch (this.type) {
-      case 'pointer':
-        this.actions.type = actionType;
-        break;
+  setState(type, subtype) {
+    switch (type) {
       case 'key':
+        this.state = {
+          pressed: [],
+          alt: false,
+          shift: false,
+          ctrl: false,
+          meta: false,
+        };
         break;
-      case 'null':
+      case 'pointer':
+        if (subtype !== 'mouse' && subtype !== 'pen' && subtype !== 'touch') {
+          throw new InvalidArgument('');
+        }
+        this.state = {
+          subtype,
+          pressed: [],
+          x: 0, // probably wont need these since we wont be implementing the mouse move action
+          y: 0,
+        };
+        break;
+      case 'none':
+        this.state = {};
         break;
       default:
         break;
