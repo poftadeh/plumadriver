fb023061b60405e2a392914049aa89d172832cbe
miguel <mroncancio19@gmail.com>
2019-07-05 17:40:13 -0400

    implement process and pause methods

--- InputSource/Actions/Action.js
+++ InputSource/Actions/Action.js
@@ -4,33 +4,80 @@ class Action {
   constructor(id, type, subtype) {
     this.id = id;
     this.type = type;
-    this.subtype = this.setActionSubType(type, subtype);
+    this.subtype = Action.setActionSubType(type, subtype);
   }
 
-  setActionSubType(type, subtype) {
+  static setActionSubType(type, subtype) {
+    let validSubtype;
     switch (type) {
       case 'pointer':
         if (
-          subtype !== 'pointerMove' &&
-          subtype !== 'pointerDown' &&
-          subtype !== 'pointerUp' &&
-          subtype !== 'pointerCancel'
+          subtype !== 'pointerMove'
+          && subtype !== 'pointerDown'
+          && subtype !== 'pointerUp'
+          && subtype !== 'pointerCancel'
+          && subtype !== 'pause'
         ) {
           throw new InvalidArgument('');
         } else {
-          this.subtype = subtype;
+          validSubtype = subtype;
         }
         break;
       case 'key':
         if (subtype !== 'keyDown' && subtype !== 'keyUp') {
           throw new InvalidArgument('');
         } else {
-          this.subtype = subtype;
+          validSubtype = subtype;
         }
+        break;
+      case 'none':
+        if (subtype !== 'pause') throw new InvalidArgument('');
         break;
       default:
         throw new InvalidArgument();
     }
+    return validSubtype;
+  }
+
+  process(actionItem) {
+    const pointer = {
+      upOrDown: () => {},
+      move: () => {},
+      cancel: () => {},
+    };
+
+    let result;
+
+    switch (this.type) {
+      case 'none':
+        result = this.pause(actionItem);
+        break;
+      case 'pointer':
+        if (this.subtype === 'pause') result = this.pause(actionItem);
+        else if (this.subtype === 'pointerMove') {
+          result = pointer.move();
+        } else if (this.subtype === 'pointerCancel') {
+          // this hasnt been updated on the w3c page... little difficult to implement w/o any specs...
+          result = pointer.cancel();
+        } else {
+          result = pointer.upOrDown();
+        }
+        break;
+      default:
+        break;
+    }
+
+    return result;
+  }
+
+  pause(actionItem) {
+    const { duration } = actionItem;
+
+    if (duration && Number.isInteger(duration) && duration >= 0) {
+      this.duration = duration;
+    } else throw new InvalidArgument('');
+
+    return this;
   }
 }
 
